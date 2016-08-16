8f563ac94b0cbeba93f02c71bfaf1f969da2f455
Brandon Faloona <brandon@faloona.net>
2016-08-16 00:55:11 -0700

    openaps-report invoke nightscout/preflight.json
          TODO: better change descriptions
          /usr/local/bin/openaps-report invoke nightscout/preflight.json

--- scripts/git-reclone.sh
+++ scripts/git-reclone.sh
@@ -14,27 +14,28 @@ if [ $? -ne 0 ]; then
 fi
 
 CORRUPTDIR="/home/pi/corrupt_repos"
-BACKUPDIR="$(CORRUPTDIR)/openaps-lp_$$"
+BACKUPDIR="$(CORRUPTDIR)/repo_$$"
+REPONAME="openaps-lp2"
 
 mkdir $CORRUPTDIR |& logger -t git-reclone
 rm -rf $BACKUPDIR |& logger -t git-reclone
 mkdir $BACKUPDIR |& logger -t git-reclone
-mv openaps-lp/* $BACKUPDIR  |& logger -t git-reclone
+mv $REPONAME/* $BACKUPDIR  |& logger -t git-reclone
 
-rm -rf openaps-lp |& logger -t git-reclone
+rm -rf openaps-lp2 |& logger -t git-reclone
 if [ $? -ne 0 ]; then
         logger -t git-reclone "GIT RECLONE FAIL - rm openaps-lp"
         exit 1
 fi
 
-git clone https://github.com/bfaloona/openaps-lp.git |& logger -t git-reclone
+git clone https://github.com/bfaloona/openaps-lp2.git |& logger -t git-reclone
 if [ $? -ne 0 ]; then
         logger -t git-reclone "GIT RECLONE FAIL - git clone"
         exit 1
 fi
 
-cp openaps-lp-assets/pump.ini openaps-lp/ |& logger -t git-reclone
-cd openaps-lp
+cp openaps-lp-assets/pump2.ini openaps-lp2/ |& logger -t git-reclone
+cd openaps-lp2
 
 git status > /dev/null
 if [ $? -eq 0 ]; then
