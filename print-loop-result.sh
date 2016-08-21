c9541202b336901e57c3fcb6279ffaa7c47445b5
Brandon Faloona <brandon@faloona.net>
2016-08-20 23:46:06 -0700

    openaps-report invoke monitor/temp-basal-status.json
          TODO: better change descriptions
          /usr/local/bin/openaps-report invoke monitor/temp-basal-status.json

--- print-loop-result.sh
+++ print-loop-result.sh
@@ -2,4 +2,8 @@ SHELL=/bin/bash
 PATH=/home/pi/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/games:/usr/games
 
 cd /home/pi/openaps-lp2
-echo PROFILE && cat oref0-monitor/profile.json && echo IOB && cat oref0-monitor/iob.json && echo PREDICT && cat oref0-predict/oref0.json && echo ENACT && cat oref0-enacted/enacted-temp-basal.json
+echo PROFILE && cat oref0-monitor/profile.json
+echo IOB && cat oref0-monitor/iob.json
+echo PREDICT && cat oref0-predict/oref0.json
+echo ENACT && cat oref0-enacted/enacted-temp-basal.json
+echo NIGHTSCOUT && cat nightscout/openaps-status.json
