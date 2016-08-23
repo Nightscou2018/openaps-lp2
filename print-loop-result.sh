bad9c8dfbb69a4c51cb18a8a5150a3eb6db2899d
Brandon Faloona <brandon@faloona.net>
2016-08-23 11:56:59 -0700

    openaps-report invoke raw-pump/pump-history-raw.json monitor/pump-history.json
          TODO: better change descriptions
          /usr/local/bin/openaps-report invoke raw-pump/pump-history-raw.json monitor/pump-history.json

--- print-loop-result.sh
+++ print-loop-result.sh
@@ -6,4 +6,5 @@ echo PROFILE && cat oref0-monitor/profile.json
 echo IOB && cat oref0-monitor/iob.json
 echo PREDICT && cat oref0-predict/oref0.json
 echo ENACT && cat oref0-enacted/enacted-temp-basal.json
+echo
 echo NIGHTSCOUT && cat nightscout/openaps-status.json
