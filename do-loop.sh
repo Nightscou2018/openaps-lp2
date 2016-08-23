3c971efe273df2e5d7a66af6af35bd1a6b90b943
Brandon Faloona <brandon@faloona.net>
2016-08-23 10:19:11 -0700

    openaps-report invoke nightscout/openaps-status.json nightscout/uploaded-recent-devicestatus.json
          TODO: better change descriptions
          /usr/local/bin/openaps-report invoke nightscout/openaps-status.json nightscout/uploaded-recent-devicestatus.json

--- do-loop.sh
+++ do-loop.sh
@@ -4,17 +4,6 @@ PATH=/home/pi/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/
 
 START=`date +%s`
 
-function error_exit
-{
-	END=`date +%s`
-	ELAPSED=$(( $END - $START ))
-	echo "$1" 1>&2
-	logger -t do-loop-error "LOOP FAIL $1"
-	./print-loop-result.sh |& logger -t do-loop-result
-	logger -t do-loop-end "OPENAPS-LP LOOP ERROR EXIT ($ELAPSED seconds)"
-        exit 1
-}
-
 logger -t do-loop-start "OPENAPS-LP LOOP START"
 
 cd /home/pi/openaps-lp2
@@ -24,7 +13,8 @@ rm -f monitor/* &>/dev/null
 if ! oref0 fix-git-corruption; then
 	logger -t do-loop-start "GIT CORRUPTION FOUND - Attempting fix"
 	if ! ./scripts/git-reclone.sh; then
-		error_exit "GIT RECLONE FAIL"
+		logger -t do-loop-error "GIT RECLONE FAIL"
+		exit 1
 	fi
 else
 	logger -t do-loop-start "LOOP PREFLIGHT"
@@ -32,7 +22,8 @@ else
         until openaps preflight;
         do
                 if [ $SECONDS -gt $preflight_timeout ]; then
-			error_exit "LOOP PREFLIGHT TIMEOUT ERROR"
+			logger -t do-loop-error "LOOP PREFLIGHT TIMEOUT ERROR"
+			exit 1
 		fi
 		sleep 10
 		logger -t do-loop-start "LOOP PREFLIGHT WAITING"
@@ -48,14 +39,7 @@ else
 	# openaps do-everything |& logger -t do-everything
 
         # Main loop
-	( openaps gather-clean-data; ) 2>&1 | logger -t do-loop-gather
-	( openaps do-oref0; ) 2>&1 | logger -t do-loop-predict
-        ( openaps enact-oref0; ) 2>&1 | logger -t do-loop-enact
-
-	# Update nightscout
-	( openaps get-basal-status; ) 2>&1 | logger -t do-loop-status
-	( openaps upload-treatments; ) 2>&1 | logger -t do-loop-status
-	( openaps upload-status; ) 2>&1 | logger -t do-loop-status
+	( openaps gather-clean-data && openaps do-oref0 && openaps enact-oref0 && openaps get-basal-status &&openaps upload-treatments && openaps upload-status ) 2>&1 | logger -t do-loop
 fi
 
 ./print-loop-result.sh |& logger -t do-loop-result
